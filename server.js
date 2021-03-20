ad3506de9aac4425d718c8b499dcafad58d26402
Lalit Narayan Mudgal <63332774+LalitNM@users.noreply.github.com>
2021-03-21 00:00:46 +0530

    In order to merge two branches (#7)
    * Added json files with api calls features
    
    * updated readme
    
    Co-authored-by: Tushar Rao <tushar.rao198@gmail.com>
    Co-authored-by: Tushar Rao <56690827+tusharrao198@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var fs = require('fs');
+const express = require('express');
+const app = express();    
+
 const PORT = 8080;
+
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +12,42 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-var server = app.listen(PORT, function() {
-    console.log(`App is live at http://127.0.0.1:${PORT}`);
+app.get('/coordinates', function (req, res) {
+    fs.readFile( "./scripts/data/coordinates.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
 });
+
+app.get('/coordinates_info', function (req, res) {
+    fs.readFile( "./scripts/data/coordinates_info.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
+});
+
+app.get('/astrosat_publications', function (req, res) {
+    fs.readFile( "./scripts/data/Astrosat_Pubs.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
+});
+
+app.get('/astrosat', function (req, res) {
+    fs.readFile( "./scripts/data/Astrosat.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
+});
+
+app.get('/', function(request, response){
+    // response.sendFile('./templates/index.html');
+    response.sendFile('index.html', { root: __dirname + '/templates' });
+});
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+// app.listen(PORT, () => console.log(`App is live`) );
+app.listen(PORT, () => console.log(`App is live at http://127.0.0.1:${PORT}`));
+
+
