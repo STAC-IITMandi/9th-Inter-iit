da4fbcdc3ce858b6bf73abcf9dbebb35c1d54cef
Tushar Rao <tushar.rao198@gmail.com>
2021-03-20 20:11:46 +0530

    Added json files with api calls features

--- scripts/plot.py
+++ scripts/plot.py
@@ -8,9 +8,10 @@ import string
 
 # mpld3.enable_notebook()
 
+
 class ClickInfo(plugins.PluginBase):
     """Plugin for getting info on click"""
-    
+
     JAVASCRIPT = """
     mpld3.register_plugin("clickinfo", ClickInfo);
     ClickInfo.prototype = Object.create(mpld3.Plugin.prototype);
@@ -30,19 +31,22 @@ class ClickInfo(plugins.PluginBase):
                     });
     }
     """
+
     def __init__(self, points, labels):
         self.labels = labels
-        self.dict_ = {"type": "clickinfo",
-                      "id": utils.get_id(points),
-                      "labels": labels}
+        self.dict_ = {"type": "clickinfo", "id": utils.get_id(points), "labels": labels}
+
 
 # plot graph here
 fig, ax = plt.subplots()
 points = ax.scatter(np.random.rand(40), np.random.rand(40), s=300, alpha=0.3)
 N = 5
 # list of source name to display corresponding to each point
-labels = ["{0}".format(''.join(random.choices(string.ascii_uppercase + string.digits, k = N))) for i in range(40)]
-
+labels = [
+    "{0}".format("".join(random.choices(string.ascii_uppercase + string.digits, k=N)))
+    for i in range(40)
+]
+print("labels", labels)
 
 tooltip = plugins.PointLabelTooltip(points, labels)
 plugins.clear(fig)  # clear all plugins from the figure
@@ -52,11 +56,11 @@ plugins.connect(fig, ClickInfo(points, labels))
 
 # print fig_id and save JS stuff to html
 js_script = mpld3.fig_to_html(fig, template_type="simple")
-tmp, js_script = js_script.split('</style>')[-1].split('</div>')
+tmp, js_script = js_script.split("</style>")[-1].split("</div>")
 tmp = tmp.strip()
-tmp = tmp[tmp.find("fig_"):-2:]
-js_script = js_script.split('\n\n', 1)[-1].split('</script>')[0].strip()
+tmp = tmp[tmp.find("fig_") : -2 :]
+js_script = js_script.split("\n\n", 1)[-1].split("</script>")[0].strip()
 print(tmp)
-js_file= open("plot.js","w")
+js_file = open("plot.js", "w")
 js_file.write(js_script)
 js_file.close()
--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 var http = require('http');
 var fs = require('fs');
+const express = require('express');
+const app = express();    
 
 const PORT = 8080;
 
-fs.readFile('./templates/index.html', function(err, html) {
-
-    if (err) throw err;
-
-    http.createServer(function(request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(PORT, () => console.log(`App is live at http://127.0.0.1:${PORT}`));
+app.get('/coordinates', function (req, res) {
+    fs.readFile( "./scripts/data/coordinates.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
 });
+
+app.get('/coordinates_info', function (req, res) {
+    fs.readFile( "./scripts/data/coordinates_info.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
+});
+
+app.get('/astrosat_publications', function (req, res) {
+    fs.readFile( "./scripts/data/Astrosat_Pubs.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
+});
+
+app.get('/astrosat', function (req, res) {
+    fs.readFile( "./scripts/data/Astrosat.json", 'utf8', function (err, data) {
+        res.end(data);
+    });
+});
+
+app.get('/', function(request, response){
+    // response.sendFile('./templates/index.html');
+    response.sendFile('index.html', { root: __dirname + '/templates' });
+});
+
+// app.listen(PORT, () => console.log(`App is live`) );
+app.listen(PORT, () => console.log(`App is live at http://127.0.0.1:${PORT}`));
