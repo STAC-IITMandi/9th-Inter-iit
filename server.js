a0a93f5ac7254c9f10e9435d31331fd95ea3aaf2
harnaman-hk <harnaman.hk@gmail.com>
2021-03-21 08:02:56 +0530

    cleanup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 const express = require('express');
-const app = express();    
+const app = express();
 
 const PORT = 8080;
 
@@ -12,42 +12,29 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/coordinates', function (req, res) {
-    fs.readFile( "./scripts/data/coordinates.json", 'utf8', function (err, data) {
+
+app.get('/coordinates', function(req, res) {
+    fs.readFile("./scripts/data/coordinates.json", 'utf8', function(err, data) {
         res.end(data);
     });
 });
 
-app.get('/coordinates_info', function (req, res) {
-    fs.readFile( "./scripts/data/coordinates_info.json", 'utf8', function (err, data) {
+app.get('/coordinates_info', function(req, res) {
+    fs.readFile("./scripts/data/coordinates_info.json", 'utf8', function(err, data) {
         res.end(data);
     });
 });
 
-app.get('/astrosat_publications', function (req, res) {
-    fs.readFile( "./scripts/data/Astrosat_Pubs.json", 'utf8', function (err, data) {
+app.get('/astrosat_publications', function(req, res) {
+    fs.readFile("./scripts/data/Astrosat_Pubs.json", 'utf8', function(err, data) {
         res.end(data);
     });
 });
 
-app.get('/astrosat', function (req, res) {
-    fs.readFile( "./scripts/data/Astrosat.json", 'utf8', function (err, data) {
+app.get('/astrosat', function(req, res) {
+    fs.readFile("./scripts/data/Astrosat.json", 'utf8', function(err, data) {
         res.end(data);
     });
 });
 
-app.get('/', function(request, response){
-    // response.sendFile('./templates/index.html');
-    response.sendFile('index.html', { root: __dirname + '/templates' });
-});
-
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-
-// app.listen(PORT, () => console.log(`App is live`) );
 app.listen(PORT, () => console.log(`App is live at http://127.0.0.1:${PORT}`));
-
-
