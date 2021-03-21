d373b367d755184b41280e7711234438bcf32134
harnaman-hk <harnaman.hk@gmail.com>
2021-03-21 08:35:10 +0530

    integrated python script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 const express = require('express');
+const { spawn } = require('child_process');
 const app = express();
 
 const PORT = 8080;
@@ -9,9 +10,26 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    var plot_id;
+    const python = spawn('python3', ['./scripts/plot.py']);
+    python.stdout.on('data', function(data) {
+        plot_id = data.toString();
+        console.log(`Retreiving stdout output of plot.py ${plot_id}`);
+        console.log(JSON.stringify(plot_id.replace(/\n/g, '')));
+        res.render('index', {
+            fid: plot_id.replace(/\n/g, '')
+        });
+    });
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+    });
 });
 
+app.get('/plot.js', function(req, res) {
+    fs.readFile("./plot.js", 'utf8', function(err, data) {
+        res.end(data);
+    });
+});
 
 app.get('/coordinates', function(req, res) {
     fs.readFile("./scripts/data/coordinates.json", 'utf8', function(err, data) {
