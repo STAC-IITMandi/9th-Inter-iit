be9fa73eb6c14cb9379502e7f599c04e8586d3d3
harnaman-hk <harnaman.hk@gmail.com>
2021-03-23 15:48:43 +0530

    updated py scripts to use any data dir

--- cat_functions.py
+++ cat_functions.py
@@ -5,15 +5,15 @@ import numpy as np
 import re
 import json
 import os
-os.chdir('data')
+# os.chdir('data')
 
 
 # This file is used to process data and returns three dataframes, for catalog A, B and C
-def catA(fname = ['hmxbcat.dat', 'lmxbcat.dat'], repDlm = [17, 24, 27, 30, 41, 44, 51, 58, 67, 72, 80, 98, 116, 130, 143, 149, 165, 184, 206, 221, 242, 259, 272, 290, 316, 329], insDlm = [36, 37, 38, 49, 122, 123, 128, 136, 137, 138, 150, 156, 157, 159, 163, 185, 196, 197, 252, 253, 257, 270, 342, 343, 344], Fields = ['Name', 'Type', 'RAh', 'RAm', 'RAs', 'u_RAs', 'DE-', 'DEd', 'DEm', 'DEs', 'u_DEs', 'GLON', 'GLAT', 'Pos', 'e_Pos', 'Opt', 'r_Opt', 'Vmag', '---', 'Vmagl', 'u_Vmag', 'B-V', 'u_B-V', '---', 'B-Vl', 'U-B', 'l_E(B-V)', 'E(B-V)', '---', 'l_E(B-V)2', 'E(B-V)2', 'u_E(B-V)', 'r_Vmag', 'l_Fx', 'Fx', '---', 'Fxu', 'Range', 'r_Fx', 'Porb', '---', 'Porb2', 'u_Porb', 'Ppulse', 'u_Ppulse', 'r_Ppulse', 'Cat', 'SpType', 'Name2',  'u_Name2', '---', 'Name3']):
+def catA(dir_name = "data", fname = ['hmxbcat.dat', 'lmxbcat.dat'], repDlm = [17, 24, 27, 30, 41, 44, 51, 58, 67, 72, 80, 98, 116, 130, 143, 149, 165, 184, 206, 221, 242, 259, 272, 290, 316, 329], insDlm = [36, 37, 38, 49, 122, 123, 128, 136, 137, 138, 150, 156, 157, 159, 163, 185, 196, 197, 252, 253, 257, 270, 342, 343, 344], Fields = ['Name', 'Type', 'RAh', 'RAm', 'RAs', 'u_RAs', 'DE-', 'DEd', 'DEm', 'DEs', 'u_DEs', 'GLON', 'GLAT', 'Pos', 'e_Pos', 'Opt', 'r_Opt', 'Vmag', '---', 'Vmagl', 'u_Vmag', 'B-V', 'u_B-V', '---', 'B-Vl', 'U-B', 'l_E(B-V)', 'E(B-V)', '---', 'l_E(B-V)2', 'E(B-V)2', 'u_E(B-V)', 'r_Vmag', 'l_Fx', 'Fx', '---', 'Fxu', 'Range', 'r_Fx', 'Porb', '---', 'Porb2', 'u_Porb', 'Ppulse', 'u_Ppulse', 'r_Ppulse', 'Cat', 'SpType', 'Name2',  'u_Name2', '---', 'Name3']):
     '''This function reads catalog A (consisting of hxmbcat.dat and lmxbcat.dat) and returns a dataframe consisting of 52 columns and 280 rows which can be used further.'''
     data = []
     for file in fname:
-        file = open(file)
+        file = open(os.path.join(dir_name, file))
         data += file.readlines()
         file.close()
     
@@ -40,15 +40,15 @@ def catA(fname = ['hmxbcat.dat', 'lmxbcat.dat'], repDlm = [17, 24, 27, 30, 41, 4
     return pd.DataFrame(data, columns = Fields).set_index(['Name'])
 
 
-def catB(fname="AS_observations_cat_Sept2018.txt",length=900,Fields=['Id','DnT','ObsId','Obs','Ra','Dec','ObsName','Name','Sat']):
+def catB(dir_name = "data", fname="AS_observations_cat_Sept2018.txt",length=900,Fields=['Id','DnT','ObsId','Obs','Ra','Dec','ObsName','Name','Sat']):
     data=[]
-    with open(fname, "r") as f:
+    with open(os.path.join(dir_name, fname), "r") as f:
         for i in range(900):
             data.append([i.strip() for i in re.split('\t|::|\n',f.readline())[:-1]])
     return pd.DataFrame(data,columns=Fields).set_index(['Id'])
 
-def catC(fname="AS_publications2019-21.txt"):
-    with open(fname,'r',encoding='utf8') as f:
+def catC(dir_name = "data", fname="AS_publications2019-21.txt"):
+    with open(os.path.join(dir_name, fname),'r',encoding='utf8') as f:
         data=[ [y for y in x.split('\n ') if y.lstrip() != ''] for x in list(f.read().split('\n\n\n'))]
 
     data_dict=[]
--- cat_json.py
+++ cat_json.py
@@ -7,9 +7,11 @@ Created on Sat Mar 20 13:12:38 2021
 
 import cat_functions
 import json
+import os
+import sys
 
-def catA_json(Fields=['Type', 'RAh', 'RAm', 'RAs', 'DE-', 'DEd', 'DEm', 'DEs', 'GLON', 'GLAT']):
-    A=cat_functions.catA()
+def catA_json(data_dir = "data", Fields=['Type', 'RAh', 'RAm', 'RAs', 'DE-', 'DEd', 'DEm', 'DEs', 'GLON', 'GLAT']):
+    A=cat_functions.catA(dir_name=data_dir)
     data=[]
     for i in range(280):
         data.append({'Name': A.index[i]})
@@ -17,11 +19,11 @@ def catA_json(Fields=['Type', 'RAh', 'RAm', 'RAs', 'DE-', 'DEd', 'DEm', 'DEs', '
             data[-1].update({field: A[field][i]})
             
     objects={"objects":data}
-    with open("Dataset.json", 'w') as f:
+    with open(os.path.join("data", "Dataset.json"), 'w') as f:
         json.dump(objects, f, indent=3)
     
-def catB_json(Fields=['Name','Ra','Dec']):
-    B=cat_functions.catB()
+def catB_json(data_dir = "data", Fields=['Name','Ra','Dec']):
+    B=cat_functions.catB(dir_name=data_dir)
     data=[]
     for i in range(900):
         data.append({})
@@ -29,25 +31,33 @@ def catB_json(Fields=['Name','Ra','Dec']):
             data[-1].update({field: B[field][i]})
             
     objects={"objects":data}
-    with open("Astrosat.json", 'w') as f:
+    with open(os.path.join("data", "Astrosat.json"), 'w') as f:
         json.dump(objects, f, indent=3)
     
     
-def catC_json():
-    publications=cat_functions.catC()
-    with open("Astrosat_Pubs.json", 'w') as f:
+def catC_json(data_dir = "data"):
+    publications=cat_functions.catC(dir_name=data_dir)
+    with open(os.path.join("data", "Astrosat_Pubs.json"), 'w') as f:
         json.dump(publications, f, indent=3)
         
 def BtoC_json():
-    B=json.load(open("Astrosat.json"))
-    C=json.load(open("Astrosat_Pubs.json"))
+    B=json.load(open(os.path.join("data", "Astrosat.json")))
+    C=json.load(open(os.path.join("data", "Astrosat_Pubs.json")))
     B = B['objects']
     C = C['publications']
     search=cat_functions.relateBtoC(B,C)
-    with open("BtoC.json", 'w') as f:
+    with open(os.path.join("data", "BtoC.json"), 'w') as f:
         json.dump(search, f, indent=3)
 
-catA_json()
-catB_json()
-catC_json()
-BtoC_json()
+if __name__ == "__main__" :
+    try:
+        data_dir_ = sys.argv[1]
+        catA_json(data_dir = data_dir_)
+        catB_json(data_dir = data_dir_)
+        catC_json(data_dir = data_dir_)
+    except:
+        catA_json()
+        catB_json()
+        catC_json()
+
+    BtoC_json()
